9014e18a4bb4c91dbe3ca2ecb56192c8f23bc50f
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2024-10-28 17:27:35 +0100

    Array lek

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const fruitArray = ['apple', 'orange', 'banana'];
+console.log(fruitArray);
+
+let enMindreFrukt = fruitArray.pop();
+console.log(enMindreFrukt);
+
+
+const fruitFågelArray = ['apple', 'orange', 'banana'];
+console.log(fruitFågelArray)
+
+fruitFågelArray.push (['druva', 'snuva']) 
+console.log(fruitFågelArray);
+
+let förstaFrukt = fruitArray.shift()
+console.log(förstaFrukt);
+
+fruitArray.unshift(['papaya', 'mango'])
+
