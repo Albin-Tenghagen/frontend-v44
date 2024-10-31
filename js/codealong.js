cea76bdd2b86a8f14781c353cbf2b92f3f842db1
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2024-10-31 11:16:19 +0100

    övning 1 färdig med rebeca och benjamin

---
+++ js/codealong.js
@@ -0,0 +1,121 @@
+console.log('Javascript loaded correctly1')
+
+//Vad är en function?
+//det är ett återanvänbart kodblock som kan återanvändas.
+
+//Deklarera och anropar man functioner?
+
+// //syntax för en att deklarera
+
+// function nameOfTheFunction(parametrar) {
+//     //Här innanflr sker koden
+//     //return result
+
+// }
+
+function sayHello() {
+    console.log('hej världen')
+}
+
+//anropa en funcion
+sayHello();
+
+//skapa en function med parametrar
+
+// function sayHello2(name) {
+//     console.log('hej världen' + name)
+// }
+
+// let promptName = prompt('säg ditt namn')
+
+// sayHello2('Albin')
+// sayHello2('Albin')
+// sayHello2('Albin')
+// sayHello2(promptName)
+// sayHello2('Thomas')
+// sayHello2('Mandus')
+
+//skapa function med en return value och varför används det?
+//return avslutar en function och spottar ut ett resultat 
+
+
+// //tidigt avslut av en function
+// function findSquareRoot(x) {
+//     if (x < 0) {
+//         return 'kan inte beräkna kvadratroten av negativa tal'
+//     } else {
+//         return Math.sqrt(x)
+//     }
+// }
+
+// console.log(findSquareRoot(9))
+// //kommer skriva ut 3
+// console.log(findSquareRoot(-11))
+// //returnerar 'kan inte beräkna kvadratroten av negativa tal'
+//------------------------------------------------------------------------
+////eftersom functionen inte har ett return värde så resulterar det i att functionen utförs, men inte att det returnerar ett värde
+function sayHello() {
+    console.log('hej världen')
+    
+}
+
+let greeting = sayHello()
+console.log(greeting)
+
+//function med två parametrar
+function add(a,b) {
+    return a + b;
+}
+console.log(add(5, 85));
+add(5, 85)
+
+//Namngiven functioner har ett namn
+//Anonyma functioner är utan namn
+//Arrow functions är den tredje sorten (=>)
+
+// //Namngiven functioner har ett namn 
+// har fördelen att dem är lätta att anropa med deras Namn, när functionens syfte är klart definierat. När det är centralt för kodens sammanhang så är namngivna fördelaktiga
+
+// //Anonyma functioner är utan namn
+// är användbara som parametrar inom en function  
+
+// // arrow functioner
+// har kortare syntax för o skriva functioner, när man har kortare functions vill man skriva det med anonyma/arrow functionerden behåller inte sitt eget this värde, vilket kan vara bra eller problematiskt, den är bra för inline functioner
+
+let person = {
+    firstName: 'Albin',
+    lastName:  'Tenghagen',
+    greeting: () => {
+        console.log('hej jag heter' + this.firstName)
+    }
+}
+console.log(person.greeting())
+//den funkar ej för this sparas ej, eftersom de inte funkar med arrow functions
+person.greeting()
+let person2 = {
+    firstName: 'Albin',
+    lastName:  'Tenghagen',
+    greeting: function greeting() {
+        console.log('hej jag heter' + this.firstName)
+    }
+}
+//exempel på en namngiven function från ovan
+
+//arrow function
+let functionName = (parameter1, parameter2) => {
+    //kod som ska utföras
+    return resultat
+}
+
+//kortare arrow function som gör samma sak som exemplet nedan
+let add2 = (a, b) => a + b;
+// samma function som den ovan skriven på ett utförligare vis 
+let add3 = (a, b) => {
+    return a + b;
+}
+
+// arrow function med en high order funtcion map()
+let num = [1, 2, 3, 4, 5, 6, 7];
+
+let multiply = num.map((num1) => num1 * 2);
+console.log(multiply)
---
+++ js/exercises.js
@@ -0,0 +1,58 @@
+// jsx24vecka44
+
+// Fördjupa JavaScript-kunskaper - Ämnen: - Functions:
+// Functions - bootcamp
+
+// Övning 1: Bryta ner kod i återanvändbara funktioner (Medel) Uppgift:
+
+// Du har följande kod som beräknar omkretsen och arean av en cirkel:
+
+// javascript Kopiera kod let radie = 5; let omkrets = 2 _ Math.PI _ radie; let area = Math.PI _ radie _ radie; console.log(Omkretsen är: ${omkrets}); console.log(Arean är: ${area}); Uppgift:
+
+function calculateArea(x) {
+    return Math.PI * (x ** 2)
+}
+
+function calculateCircumference(x) {
+    return Math.PI * 2 * x
+}
+console.log(calculateCircumference(radie))
+console.log(calculateArea(radie))
+
+// Bryt ner koden i två separata funktioner: beräknaOmkrets(radie) och beräknaArea(radie). Använd funktionerna för att beräkna omkretsen och arean för cirklar med olika radier. Skriv ut resultaten. Mål:
+
+// Öva på att skapa och anropa funktioner med parametrar och returvärden. Förstå fördelarna med att återanvända kod. Övning 2: Förstå scope och variabeltillgänglighet (Medel) Uppgift:
+
+// Studera följande kod:
+
+// javascript Kopiera kod let x = 10;
+
+// function ändraX() { let x = 20; console.log(Inuti funktionen, x = ${x}); }
+
+// ändraX(); console.log(Utanför funktionen, x = ${x}); Frågor:
+
+// Vad kommer att skrivas ut av console.log()-satserna? Ändras värdet av x utanför funktionen? Modifiera koden så att funktionen faktiskt ändrar värdet av x i det globala scopet. Mål:
+
+// Förstå skillnaden mellan lokala och globala variabler. Lära sig hur scope påverkar variabeltillgänglighet. Övning 3: Anonyma funktioner och arrow functions (Svår) Uppgift:
+
+// Skapa en array av tal: [1, 2, 3, 4, 5]. Använd setTimeout() och en anonym funktion för att skriva ut varje tal med en fördröjning på 1 sekund mellan varje utskrift. Modifiera koden för att använda arrow functions istället för anonyma funktioner. Förklara hur closure påverkar detta exempel. Mål:
+
+// Förstå användningen av anonyma funktioner och arrow functions i asynkrona sammanhang. Lära sig om closures och hur variabler bevaras i funktioner.
+
+// Övning 4: Rekursiva Funktioner (Svår) Uppgift:
+
+// Skriv en rekursiv funktion fakultet(n) som beräknar n! (n fakultet). Testa funktionen med olika värden av n. Förklara hur rekursion fungerar i detta sammanhang. Mål:
+
+// Förstå konceptet av rekursion och hur man implementerar det i JavaScript. Lära sig att tänka algoritmiskt för att lösa problem. Övning 5: Scope och Hoisting (Svår) Uppgift:
+
+// Analysera följande kod:
+
+// javascript Kopiera kod console.log(x); var x = 5;
+
+// funktion();
+
+// function funktion() { console.log("Inuti funktionen"); } Frågor:
+
+// Vad kommer att skrivas ut av console.log(x)? Varför fungerar anropet till funktion() innan den är definierad? Modifiera koden genom att använda let istället för var och observera skillnaderna. Mål:
+
+// Förstå konceptet av hoisting i JavaScript. Lära sig skillnaderna mellan var, let och const.
